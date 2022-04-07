d8059566673869ab5f29dd9fcd78fa3a71bdeedf
120-seto <ks10c135@gmail.com>
2022-04-07 12:36:03 +0900

    お名前(全角)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  #validates :first_name, presence: true
+  #validates :last_name, presence: true
+  validates :first_name,
+   format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/, message: "は全角文字を使用してください"},
+   presence: true
+ validates :last_name,
+   format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/, message: "は全角文字を使用してください"},
+   presence: true
+  #validates :first_name_kana, presence: true
+  #validates :last_name_kana, presence: true
+  validates :first_name_kana,
+  format: { with: /\A([ァ-ン]|ー)+\z/, message: "は全角（カタカナ）での入力が必須であること。"},
+  presence: true
+validates :last_name_kana,
+  format: { with: /\A([ァ-ン]|ー)+\z/, message: "全角は（カタカナ）での入力が必須であること。"},
+  presence: true
   validates :birthday, presence: true
+
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: "は半角英数を両方含む必要があります"}
+  
 end
