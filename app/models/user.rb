e67868929605923cef73422a51b7fb8185170f33
120-seto <ks10c135@gmail.com>
2022-04-07 15:48:59 +0900

    コメントアウト部分削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  #validates :first_name, presence: true
-  #validates :last_name, presence: true
   validates :first_name,
    format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/, message: "は全角文字を使用してください"},
    presence: true
  validates :last_name,
    format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/, message: "は全角文字を使用してください"},
    presence: true
-  #validates :first_name_kana, presence: true
-  #validates :last_name_kana, presence: true
   validates :first_name_kana,
   format: { with: /\A([ァ-ン]|ー)+\z/, message: "は全角（カタカナ）での入力が必須であること。"},
   presence: true
