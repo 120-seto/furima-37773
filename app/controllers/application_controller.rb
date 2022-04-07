e6c1993181f32c1c577dd95b1462787a25ac0798
120-seto <ks10c135@gmail.com>
2022-04-07 15:35:54 +0900

    ログイン可能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,6 @@ class ApplicationController < ActionController::Base
   end
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
-  end
+    #devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+ end
 end
