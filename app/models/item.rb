11c43404d6ac89b6e147e6b00fbb214f39a8829e
120-seto <ks10c135@gmail.com>
2022-04-11 17:44:08 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :customer
+ 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :shipping_cost_id, presence: true
+  validates :shipping_area_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :price, presence: true
 end
