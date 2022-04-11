7f6001f93db8593a220bb233fcd8ebf076dc9c80
120-seto <ks10c135@gmail.com>
2022-04-11 14:41:22 +0900

    テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -72,11 +72,36 @@ RSpec.describe User, type: :model do
           @user.valid?
           expect(@user.errors.full_messages).not_to include('は半角英数を両方含む必要があります')
         end
+        it 'パスワードが全角文字を含むと登録できない' do
+          @user.password = 'AAA111'
+          @user.valid?
+          expect(@user.errors.full_messages).not_to include('は半角英数を両方含む必要があります')
+        end
         it 'passwordとpassword_confirmationが不一致では登録できない' do
           @user.password = '123456'
           @user.password_confirmation = '1234567'
           @user.valid?
           expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'first_nameが空では登録できない' do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).not_to include("First name 全角ひらがな、全角カタカナ、漢字で入力して下さい")
+      end
+      it 'last_nameが空では登録できない' do
+        @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).not_to include("Last name 全角ひらがな、全角カタカナ、漢字で入力して下さい")
+      end
+      it 'first_name_kanaが空では登録できない' do
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).not_to include("First name kana 全角カタカナで入力して下さい")
+      end
+      it 'last_name_kanaが空では登録できない' do
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).not_to include("First name kana 全角カタカナで入力して下さい")
       end
       it 'first_nameが半角では登録できない' do
         @user.first_name = 'aaaa'
