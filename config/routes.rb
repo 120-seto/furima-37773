2998d9dfa3a09125baddd6e3f02731b511076462
120-seto <ks10c135@gmail.com>
2022-04-12 03:38:13 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   get 'articles/new'
   root to: 'articles#index'
   resources :articles
+  devise_for :users
+ root 'items#index'
+
+ resources :items, only: [:index, :new, :create]
 end
