f83322a9f8e885dc14803df93037000d255aba74
120-seto <ks10c135@gmail.com>
2022-04-12 02:57:43 +0900

    Active Storageの導入-2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,10 @@ class ItemsController < ApplicationController
   end
   def create
   end
+
+  private
+
+  def message_params
+    params.require(:item).permit(:customer, :image).merge(user_id: current_user.id)
+  end
 end
