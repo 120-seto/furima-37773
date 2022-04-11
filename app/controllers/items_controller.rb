65e9f20b265b66942e4da22f1f19316a0928b3f4
120-seto <ks10c135@gmail.com>
2022-04-12 04:21:03 +0900

    ActiveHashの導入場所間違えたため削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,29 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+
   def index
+    @items = Item.all
   end
+
   def new
+    @item = Item.new
   end
+  
   def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+
 
   private
 
-  def message_params
-    params.require(:item).permit(:customer, :image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_cost_id, :shipping_area_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
