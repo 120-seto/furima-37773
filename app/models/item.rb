9e4bdf666dc45a1e5fac8adac34e16b3b0f6d082
120-seto <ks10c135@gmail.com>
2022-04-12 12:24:23 +0900

    ActiveHashの導入、カテゴリー作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,11 @@ class Item < ApplicationRecord
   validates :shipping_area_id, presence: true
   validates :shipping_day_id, presence: true
   validates :price, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+ end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_11_180959) do
+ActiveRecord::Schema.define(version: 2022_04_12_024722) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,14 +33,6 @@ ActiveRecord::Schema.define(version: 2022_04_11_180959) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "text", null: false
-    t.integer "genre_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
@@ -54,6 +46,12 @@ ActiveRecord::Schema.define(version: 2022_04_11_180959) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
