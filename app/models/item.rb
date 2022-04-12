21e0a567f82cec00751281f343418c3dd08d829b
120-seto <ks10c135@gmail.com>
2022-04-13 06:27:54 +0900

    商品情報がデータベースに保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   has_one :customer
   has_one_attached :image
 
+  validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true
   validates :category_id, presence: true
