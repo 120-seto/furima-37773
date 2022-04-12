a83dfcc6890978561ec33ac6a846818eab7ddae1
120-seto <ks10c135@gmail.com>
2022-04-12 12:33:49 +0900

    商品の状態作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,8 +14,9 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-
+  belongs_to :status
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
+    validates :status_id
  end
 end
