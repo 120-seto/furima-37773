7878eca7aed4138b4e876f72dad9fb21b7d544c4
120-seto <ks10c135@gmail.com>
2022-04-12 12:44:09 +0900

    配送料の負担作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,8 +15,10 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
+  belongs_to :shipping_cost
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
     validates :status_id
+    validates :shipping_cost_id
  end
 end
