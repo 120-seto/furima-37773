48865c7160eba0897c4fe0fe2557226498c860c1
120-seto <ks10c135@gmail.com>
2022-04-15 21:26:09 +0900

    商品情報編集ページへ遷移しない設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :ensure_current_user, only: [:edit, :update]
+  #before_action :prevent_url, only: [:index, :create]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -41,4 +43,15 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_cost_id, :shipping_area_id,
                                  :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
+  def ensure_current_user
+    item = Item.find(params[:id])
+    if item.user_id != current_user.id
+      redirect_to root_path
+    end
+  end
+  #def prevent_url
+    #if item.user_id != current_user.id || @item.customer != nil
+      #redirect_to root_path
+   # end
+ # end
 end
