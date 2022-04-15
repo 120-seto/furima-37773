695983e6b9306c8f8d4ef9e7fe444cabe703bbb1
120-seto <ks10c135@gmail.com>
2022-04-15 21:59:11 +0900

    Rubocopの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :ensure_current_user, only: [:edit, :update]
-  #before_action :prevent_url, only: [:index, :create]
+  # before_action :prevent_url, only: [:index, :create]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -32,7 +32,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
-     else
+    else
       render :edit
     end
   end
@@ -43,15 +43,14 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_cost_id, :shipping_area_id,
                                  :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
+
   def ensure_current_user
     item = Item.find(params[:id])
-    if item.user_id != current_user.id
-      redirect_to root_path
-    end
+    redirect_to root_path if item.user_id != current_user.id
   end
-  #def prevent_url
-    #if item.user_id != current_user.id || @item.customer != nil
-      #redirect_to root_path
-   # end
- # end
+  # def prevent_url
+  # if item.user_id != current_user.id || @item.customer != nil
+  # redirect_to root_path
+  # end
+  # end
 end
