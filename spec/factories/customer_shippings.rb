368942928352a8c2d7efd0c8d3ac336fab32c088
120-seto <ks10c135@gmail.com>
2022-04-19 05:11:22 +0900

    FactoryBot設定

--- spec/factories/customer_shippings.rb
+++ spec/factories/customer_shippings.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :customer_shippings do
-    
+    post_code { '123-4567' }
+    shipping_area_id { 2 }
+    city { '東京都' }
+    address { '1-1' }
+    building_name { '東京ハイツ' }
+    phone_number { 090-12345678 }
   end
 end
