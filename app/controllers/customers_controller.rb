ab63126722c660bd0df3398d28582e5237a53694
120-seto <ks10c135@gmail.com>
2022-04-18 16:54:10 +0900

    購入画面の表示名変更

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,7 @@
 class CustomersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
     @customer_shipping = CustomerShippings.new
   end
 
@@ -16,6 +17,6 @@ class CustomersController < ApplicationController
   private
 
   def customer_params
-    params.require(:customer_shippings).permit(:post_code, :shipping_area_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id)
+    params.require(:customer_shippings).permit(:post_code, :shipping_area_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id,item_id:params[:item_id])
   end
 end
