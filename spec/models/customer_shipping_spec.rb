aa33121c7583fdbac34133db0e7eb3ba2aadb206
120-seto <ks10c135@gmail.com>
2022-04-19 15:28:56 +0900

    テストコード（Formオブジェクトまで）

--- spec/models/customer_shipping_spec.rb
+++ spec/models/customer_shipping_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe CustomerShipping, type: :model do
   describe '購入情報の保存' do
     before do
       user = FactoryBot.create(:user)
-      @item = FactoryBot.build(:item)
-      @customer_shipping = FactoryBot.build(:customer_shipping, user_id: user.id, item_id: @item.id)
+      item = FactoryBot.create(:item)
+      @customer_shipping = FactoryBot.build(:customer_shipping, user_id: user.id, item_id: item.id)
+      sleep(1)
     end
+    
 
     context '内容に問題ない場合' do
       it 'すべての値が正しく入力されていれば保存できること' do
