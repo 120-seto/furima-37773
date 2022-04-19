e343d7f8047489e1299010f57262980282e61294
120-seto <ks10c135@gmail.com>
2022-04-20 05:37:21 +0900

    pay_iem作成

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -9,8 +9,9 @@ class CustomersController < ApplicationController
     @item = Item.find(params[:item_id])
     @customer_shipping =CustomerShipping.new(customer_params)
     if @customer_shipping.valid?
+      pay_item
       @customer_shipping.save
-      redirect_to root_path
+      return redirect_to root_path
     else
       render :index
     end
@@ -18,6 +19,15 @@ class CustomersController < ApplicationController
   private
 
   def customer_params
-    params.require(:customer_shipping).permit(:post_code, :shipping_area_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id,item_id:params[:item_id])
+    params.require(:customer_shipping).permit(:post_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :price).merge(user_id: current_user.id,item_id:params[:item_id],token: params[:token])
   end
+
+  def pay_item
+  Payjp.api_key = "sk_test_d1dab60c6f31f17d1b85ffe7"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+  Payjp::Charge.create(
+    amount: customer_params[:price],  # 商品の値段
+    card: customer_params[:token],    # カードトークン
+    currency: 'jpy'                 # 通貨の種類（日本円）
+  )
+ end
 end
