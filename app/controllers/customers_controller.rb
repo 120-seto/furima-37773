84db39929a087bde855ae1224c3a94967eca2907
120-seto <ks10c135@gmail.com>
2022-04-22 11:45:12 +0900

    商品購入ページに遷移時の条件分岐

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,13 +1,26 @@
 class CustomersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!
+  before_action :move_to_signed_in, except: :index
+
   def index
+    @item = Item.find(params[:item_id])
+    @customer_shipping = CustomerShipping.new
+    if current_user == @item.user
+      redirect_to root_path
+  end
+  if @item.user_id != current_user.id
+    redirect_to root_path
+  end
+end
+
+  def new
     @item = Item.find(params[:item_id])
     @customer_shipping = CustomerShipping.new
   end
 
   def create
     @item = Item.find(params[:item_id])
-    @customer_shipping =CustomerShipping.new(customer_params)
+    @customer_shipping = CustomerShipping.new(customer_params)
     if @customer_shipping.valid?
       pay_item
       @customer_shipping.save
@@ -19,15 +32,21 @@ class CustomersController < ApplicationController
   private
 
   def customer_params
-    params.require(:customer_shipping).permit(:post_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :price).merge(user_id: current_user.id,item_id:params[:item_id],token: params[:token])
+    params.require(:customer_shipping).permit(:post_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :price).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
-  Payjp.api_key = "sk_test_d1dab60c6f31f17d1b85ffe7"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
-  Payjp::Charge.create(
-    amount: customer_params[:price],  # 商品の値段
-    card: customer_params[:token],    # カードトークン
-    currency: 'jpy'                 # 通貨の種類（日本円）
-  )
- end
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: customer_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
+
+  def move_to_signed_in
+    unless user_signed_in?
+      redirect_to  root_path
+    end
+  end
 end
